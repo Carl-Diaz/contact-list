cbc77608933350a88939476f76cee5bbf115f6ba
alberto diaz <albersan338@gmail.com>
2023-09-18 08:44:52 -0500

    Actividad 2 lista de contacto ACTUALIZACION

--- contactos.js
+++ contactos.js
@@ -1,22 +1,53 @@
 let listaContacto = [
-  "Carlos Diaz",
-  "Jhon Pitre",
-  "Juan Garcia",
-  "Pedro Paramo",
+  {
+    id: 1,
+    nombre: "carlos",
+    apellido: "Diaz",
+    telefono: 3152740789,
+    ubicacion: "Colombia",
+    ciudad: "Fonseca- La-Guajira",
+    direccion: "Calla 20 #59b-75",
+  },
+  {
+    id: 2,
+    nombre: "Juan",
+    apellido: "Pitre",
+    telefono: 3001569442,
+    ubicacion: "Colombia",
+    ciudad: "Medellin",
+    direccion: "Tranversal 19 #12c-91",
+  },
+  {
+    id: 3,
+    nombre: "Wendys",
+    apellido: "Rios",
+    telefono: 3207648930,
+    ubicacion: "Colombia",
+    ciudad: "Bogota",
+    direccion: "Diagonal 5 #47aa-134",
+  },
 ];
-
-function agregarContacto(nuevoContacto) {
-  listaContacto.push(nuevoContacto);
+function añadirContacto(contacto) {
+  listaContacto.push(contacto);
 }
 
-agregarContacto("Maria Perez");
+let nuevoContacto = {
+  id: 4,
+  nombre: "Kevin",
+  apellido: "Zapata",
+  telefono: 3205628746,
+  ubicacion: "Colombia",
+  ciudad: "Barranquilla",
+  direccion: "Carrera 21 #72-87",
+};
 
-function borrarContacto(borrar) {
-  listaContacto.splice(borrar, 1);
-}
+añadirContacto(nuevoContacto);
 
-function imprimirContacto() {
-  for (let i = 0; i < listaContacto.length; i++) {
-    console.log(listaContacto[i]);
+function chaoContacto(chao) {
+  const posicion = listaContacto.indexOf(chao);
+  if (posicion !== -1) {
+    listaContacto.splice(posicion, 1);
   }
 }
+chaoContacto(listaContacto[1]);
+console.log(listaContacto);
